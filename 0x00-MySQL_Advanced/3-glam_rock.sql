d4680141bb77d71a73af1c712a253ae299a383f0
rawi daniel <rawidaniel96@gmail.com>
2023-01-15 08:37:04 +0000

    fix error

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,5 +2,4 @@
 SELECT band_name,
           IFNULL(split,2020) - IFNULL(formed,0) AS lifespan
 	FROM metal_bands
-	WHERE style like '%Glam rock%'
-	ORDER BY 2 DESC;
+	WHERE style like '%Glam rock%';
